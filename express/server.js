274178de179ecc5304bf1357767bf1459913efbe
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2020-09-03 23:55:09 +0700

    add endpoint /css

--- express/server.js
+++ express/server.js
@@ -5,6 +5,14 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
+const AvailableFonts = require(path.join(__dirname, '../public/lib/available-fonts'));
+global.fonts = new AvailableFonts();
+
+const FontQueryStringParser = require(path.join(__dirname, '../public/lib/font-query-string-parser'));
+const setCorsHeaders = require(path.join(__dirname, '../public/lib/set-cors-headers'));
+const fontFaceTemplate = fs.readFileSync((path.join(__dirname, '../public/css/font-face-template.css')), 'utf8');
+
+
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -14,9 +22,47 @@ router.get('/', (req, res) => {
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
+app.set('etag', false);
+app.set('x-powered-by', false);
+app.use(require('lib/static-assets-middleware'));
+
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
+app.get("/css", function(req, res, next) {
+  setCorsHeaders(res);
+  res.set("x-frame-options", "SAMEORIGIN");
+  res.set("content-type", "text/css");
+  res.set("cache-control", "private, max-age=86400, stale-while-revalidate=604800");
+
+  var parser = new FontQueryStringParser(req.query.family);
+  if (!parser.isQueryValid()) {
+    res.send(parser.queryValidationErrors());
+    return next();
+  }
+  if (!parser.areFamiliesValid()) {
+    res.send(parser.familyValidationErrors());
+    return next();
+  }
+  if (!parser.areVariantsValid()) {
+    res.send(parser.variantValidationErrors());
+    return next();
+  }
+
+  var css = "";
+  parser.fonts.forEach(function(font) {
+    font.variants.forEach(function(variant) {
+      css += interpolate(fontFaceTemplate, {
+        fontFamily: variant.family,
+        fontUrl: baseUrl + variant.path(),
+        fontWeight: variant.weight,
+        fontStyle: variant.style,
+      });
+    });
+  });
+
+  res.send(css);
+});
 module.exports = app;
 module.exports.handler = serverless(app);

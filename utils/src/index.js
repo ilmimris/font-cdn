0eb94a84bca08b6c9ef2a0a8dcdf2c2cb4446b15
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2020-09-04 16:11:47 +0700

    clean up repo

--- utils/src/index.js
+++
@@ -1,6 +0,0 @@
-export { AvailableFonts } from './available-fonts';
-export { Font } from './font';
-export { FontQueryStringParser } from './font-query-string-parser';
-export { FontVariant } from './font-variant';
-export { default as SetCorsHeaders } from './set-cors-headers';
-export { default as StaticAssetMiddleware } from './static-assets-middleware';
